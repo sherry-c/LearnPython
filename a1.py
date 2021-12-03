decfe4e805186fd7c0308cda9aa0082f75eb24e2
Sherry <Sherry@gamil.com>
2021-12-03 21:47:24 +0800

    创建第4章学习目录，python对象类型

---
+++ a1.py
@@ -0,0 +1,8 @@
+from PySide6.QtGui import QIcon,QImage
+from PySide6.QtCore import Qt,QThread
+from PySide6.QtWidgets import QApplication,QWidget,QPushButton,QGridLayout
+
+import sys
+
+if __name__=='__main__':
+    print(sys.argv)
