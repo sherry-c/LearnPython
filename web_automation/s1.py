0e75ed7ab44b49eb873f738fb92f7b435823d9f0
sherry <11721239+sherry-auto@user.noreply.gitee.com>
2025-07-06 18:42:45 +0800

    3

--- web_automation/s1.py
+++ web_automation/s1.py
@@ -1,9 +1,13 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import os
 
 # 创建 WebDriver 对象，指明使用chrome浏览器驱动
-wd = webdriver.Chrome(service=Service(r'C:\Users\15516\Desktop\projects\LearnPython\web_automation\chromedriver-win64\chromedriver.exe'))
+driver_path = os.path.join(os.path.dirname(__file__), 'chromedriver-win64', 'chromedriver.exe')
+
+wd = webdriver.Chrome(service=Service(driver_path))
+
 
 # 调用WebDriver 对象的get方法 可以让浏览器打开指定网址
 wd.get('https://www.baidu.com')
